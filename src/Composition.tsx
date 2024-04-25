cf3040bd77f0db1d2086243a9cec0c4bbbcd6e7c
Devessier <baptiste@devessier.fr>
2024-04-25 12:02:59 +0200

    feat: scaffold the transition series

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,8 +1,27 @@
-import {AbsoluteFill} from 'remotion';
+import {TransitionSeries, linearTiming} from '@remotion/transitions';
+import {AbsoluteFill, Img, OffthreadVideo, staticFile} from 'remotion';
 import {z} from 'zod';
+import {fadeScale} from './fade-scale-transition';
 
 export const schema = z.object({});
 
 export const MyComposition: React.FC<z.infer<typeof schema>> = () => {
-	return <AbsoluteFill className="bg-gray-100" />;
+	return (
+		<TransitionSeries>
+			<TransitionSeries.Sequence durationInFrames={10}>
+				<Img src={staticFile('/thumbnail.png')} />
+			</TransitionSeries.Sequence>
+
+			<TransitionSeries.Transition
+				presentation={fadeScale({})}
+				timing={linearTiming({
+					durationInFrames: 10,
+				})}
+			/>
+
+			<TransitionSeries.Sequence durationInFrames={120}>
+				<OffthreadVideo src={staticFile('/short.mp4')} />
+			</TransitionSeries.Sequence>
+		</TransitionSeries>
+	);
 };
